cb0d8f64408c7e116956c18143f1ba338abcbe52
palagen <padalkapavel@gmail.com>
2016-02-21 23:07:03 +0300

    Initial commit

---
+++ src/main/java/com/palagen/secure/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.palagen.secure.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
